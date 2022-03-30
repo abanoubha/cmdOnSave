0ff4c83215832126043d1ab890b2d2e9a146fc8c
Abanoub Hanna <abanoubhannaa73@gmail.com>
2022-03-30 13:18:31 +0200

    v0.1 but not working yet

---
+++ README.md
@@ -0,0 +1,37 @@
+# cmdOnSave
+
+WIP pre-alpha stage !
+
+- use in-project `cmdOnSave.json` file to add the command(s) you want to run on save.
+- you can run a specific command on save for each file type, or you can run command(s) after saving any file in the current project.
+
+[cmdOnSave](https://github.com/abanoubha/cmdOnSave) is inspired by [emeraldwalk's runOnSave](https://github.com/emeraldwalk/vscode-runonsave).
+
+## Example config file
+
+Config file is stored in the root of the project as `cmdOnSave.json`.
+
+```json
+{
+    "commands": [
+        {
+            "match": ".*",
+            "isAsync": true,
+            "cmd": "npx tailwindcss -i ./src/input.css -o ./public/style.css --minify"
+        },
+        {
+            "match": "\\.html$",
+            "cmd": "npx tailwindcss -i ./src/input.css -o ./public/style.css --minify",
+            "isAsync": true
+        },
+        {
+            "match": "\\.css$",
+            "cmd": "npx tailwindcss -i ./src/input.css -o ./public/style.css --minify"
+        }
+    ]
+}
+```
+
+## Tasks
+
+- [x] bare bone extension <https://code.visualstudio.com/api/get-started/your-first-extension>
---
+++ extension.js
@@ -0,0 +1,63 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	vscode.workspace.onDidSaveTextDocument((document) => {
+		// vscode.window.setStatusBarMessage(document.fileName);
+
+		var cfgPath = vscode.workspace.getWorkspaceFolder(document.uri);
+		cfgPath = cfgPath ? cfgPath.uri.fsPath : vscode.workspace.workspaceFolders;
+
+		var cfg = vscode.workspace.findFiles("file://" + cfgPath + "/cmdOnSave.json")
+			.then(result => vscode.workspace.fs.readFile(result[0]));
+
+		console.log(cfg);
+
+		vscode.window.setStatusBarMessage(cfgPath);
+	});
+
+	// config = vscode.workspace.findFiles('**​/cmdOnSave.json', '**​/node_modules/**', 1)
+	// 		.then(result => vscode.workspace.fs.readFile(result[0]));
+
+	// vscode.workspace.fs.readFile(vscode.Uri.parse(context.asAbsolutePath('package.json')))
+
+	// vscode.window.setStatusBarMessage(message);
+
+	// public showOutputMessage(message?: string): void {
+	// 	message = message || `Cmd On Save ${this.isEnabled ? 'enabled' : 'disabled'}.`;
+	// 	this._outputChannel.appendLine(message);
+	// }
+
+	// let disposable = vscode.commands.registerCommand('cmdonsave.helloWorld', function () {
+	// 	vscode.window.showInformationMessage('Hello World from cmdOnSave!');
+	// });
+	// context.subscriptions.push(disposable);
+
+	vscode.commands.registerCommand('extension.abanoubha.enableCmdOnSave', () => {
+		// context.globalState.update('isEnabled', true);
+		// const msg = `Cmd On Save is ${this.isEnabled ? 'enabled' : 'disabled'}.`;
+		vscode.OutputChannel.appendLine("Cmd On Save is enabled");
+	});
+
+	vscode.commands.registerCommand('extension.abanoubha.disableCmdOnSave', () => {
+		// context.globalState.update('isEnabled', false);
+		// const msg = `Cmd On Save is ${this.isEnabled ? 'enabled' : 'disabled'}.`;
+		vscode.OutputChannel.appendLine("Cmd On Save is disabled");
+	});
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
